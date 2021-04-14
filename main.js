b52ce0875da4844508e4e1d9a25bfa32df9cb923
Giuseppe <festagiuseppe89@gmail.com>
2021-04-14 15:31:39 +0200

    footer_copyrigth

---
+++ main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        nav_links: {
+            name: ['home', 'about', 'services', 'team', 'blog'],
+
+            footer: {
+                info: {
+                    
+                }
+            }
+        },
+    }
+});
+
+
+// nav_lists: {
+//     head : ['home', 'about', 'departments', 'articles'],
+
+//     footer : {
+//         infos : {
+//             location : '121 King Street, London United Kingdom',
+//             phone : '1.800.458.556' 
+//         },
+        
+//         departments : ['Cardiology', 'Pediatrics', 'Diabetes Care', 'Pre-natel Care','Ultrasound Echocardiogram'],
+
+//         credits : ['© Copyright 2012-2020','Avada theme by', 'All Rights Reserved', 'Powered by ']
+//     }
+// },    
